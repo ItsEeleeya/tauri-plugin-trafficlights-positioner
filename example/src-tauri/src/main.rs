94ce1d3fe19ad0e13795094a2adc07085a623fd7
Ilya <47112191+ItsEeleeya@users.noreply.github.com>
2024-08-02 23:02:46 +0330

    Update example to include important note.

--- example/src-tauri/src/main.rs
+++ example/src-tauri/src/main.rs
@@ -7,12 +7,11 @@ use tauri_plugin_trafficlights_positioner::WindowExt;
 fn main() {
     tauri::Builder::default()
         .setup(move |app| {
-            let window = app.get_window("main").expect("failed to get main window");
-
-            #[cfg(target_os = "macos")]
-            {
+            if let Some(window) = app.get_window("main") {
+                #[cfg(target_os = "macos")]
+                // NOTE: Make sure you only call this ONCE per window.
                 let _ = window.setup_traffic_lights_inset(LogicalPosition::new(20.0, 24.0));
-            }
+            };
 
             Ok(())
         })
