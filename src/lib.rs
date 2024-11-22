dbf0f004df8cfb574eb674d777f3c160c898658d
Victor Aremu <me@victorare.mu>
2024-11-22 21:08:44 +0100

    feat: v2

--- src/lib.rs
+++ src/lib.rs
@@ -8,10 +8,6 @@ mod positioner;
 extern crate cocoa;
 
 #[cfg(target_os = "macos")]
-#[macro_use]
-extern crate objc;
-
-#[cfg(target_os = "macos")]
 pub trait WindowExt {
     fn setup_traffic_lights_inset(&self, offset: LogicalPosition<f64>) -> tauri::Result<()>;
 }
@@ -20,15 +16,14 @@ pub trait WindowExt {
 impl<R: Runtime> WindowExt for Window<R> {
     fn setup_traffic_lights_inset(&self, inset: LogicalPosition<f64>) -> tauri::Result<()> {
         let win = self.clone();
-        self.on_window_event(move |event| match event {
-            tauri::WindowEvent::ThemeChanged(_) => {
+        self.on_window_event(move |event| {
+            if let tauri::WindowEvent::ThemeChanged(_) = event {
                 let win_clone = win.clone();
 
                 let _ = win.clone().run_on_main_thread(move || {
                     positioner::update(&win_clone, &inset);
                 });
             }
-            _ => (),
         });
 
         let win = self.clone();
